6e5e5969f7625d4bc85a560da7bbf394ab19fd42
ho1messi <1017145733@qq.com>
2016-12-11 20:52:29 +0800

    16/12/11 添加部分注释， 假设已知法向量，增加包围盒合并

--- OpenGL_05/RBF_Func.h
+++ OpenGL_05/RBF_Func.h
@@ -14,11 +14,16 @@ using std::string;
 using glm::vec3;
 using glm::vec4;
 
-class NoSuitBoxError {};
+struct RBF_Point
+{
+	vec3 point;
+	float w;
+	float d;
+};
 
 struct BoundingBox
 {
-	std::list<vec4 *> points;
+	std::list<RBF_Point *> points;
 	float xMin;
 	float xMax;
 	float xMid;
@@ -33,19 +38,21 @@ struct PointAndNormal
 	vec3 point;
 
 	// easy to multyply by vec4
-	vec4 Normal;
+	vec3 normal;
 };
 
+typedef std::list<RBF_Point *> RBF_Point_List;
 typedef std::list<BoundingBox *> RBF_BBox_List;
 typedef std::list<PointAndNormal *> RBF_PointNormal_List;
 typedef RBF_BBox_List::iterator RBF_BBox_Iter;
 typedef RBF_PointNormal_List::iterator RBF_PointNormal_Iter;
+typedef RBF_Point_List::iterator RBF_Point_Iter;
 
 // build the infinity float num from bitmap
 const unsigned int P_INFINITY_F_BITMAP = 0x7F7FFFFF;
 const float INFINITY_F = *((float *)(&P_INFINITY_F_BITMAP));
 
-const float BOUNDING_BOX_THICK = 0.001f;
+const float BOUNDING_BOX_THICK = 0.0f;
 const float BBOX_MIN_HALF_WIDTH = 0.0001f;
 
 class RBF_Func
@@ -74,6 +81,7 @@ private:
 	void addNewPoints(RBF_PointNormal_List * pointNormals, BoundingBox * box, float pointDistance);
 
 	inline void divBBoxByX(BoundingBox * box1, BoundingBox * box2);
+	inline void mergeBBox(BoundingBox * box1, BoundingBox * box2);
 	
 	inline void bBoxPointsWeight(BoundingBox * box);
 	inline void bBoxInfo(BoundingBox * box);
@@ -82,7 +90,7 @@ private:
 	inline float bBoxFunc(BoundingBox * box, float x, float y, float z);
 	inline float bBoxFuncWeight(BoundingBox * box, float x);
 
-	inline static float vec3DisModuleCube(const vec4 * point1, const vec4 * point2);
+	inline static float vec3DisModuleCube(const vec3 & point1, const vec3 & point2);
 };
 
 bool bBoxCmp(const BoundingBox * box1, const BoundingBox * box2);
