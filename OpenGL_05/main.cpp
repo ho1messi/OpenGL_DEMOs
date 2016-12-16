dc7dc2f16ef60a317e4f867875bc5b5ddde49fd6
ho1messi <1017145733@qq.com>
2016-12-16 12:43:25 +0800

    16/12/16 修正矩阵维数

--- OpenGL_05/main.cpp
+++ OpenGL_05/main.cpp
@@ -232,7 +232,7 @@ void scroll_callback(GLFWwindow *window, double xOffset, double yOffset)
 void drawInit()
 {	
 	//MCmesh = new MC_Mesh<NUM_OF_CUBES>(&f1);
-	MCmesh = new PC_Mesh<NUM_OF_CUBES>("Resource\\vertices_test.txt");
+	MCmesh = new PC_Mesh<NUM_OF_CUBES>("Resource\\vertices0.txt");
 	HESmesh = MCmesh->getMesh();
 	HESmeshSubdivition = new HES_MeshSubdivition(HESmesh);
 	//HESmesh->readFromObj("Resource\\mannequin.obj");
